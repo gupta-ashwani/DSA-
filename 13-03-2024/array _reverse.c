289fd559fb834a03cdb74eb5a93523dd9f9a8c38
Ashwani Gupta <161638810+gupta-ashwani@users.noreply.github.com>
2024-03-13 14:47:47 +0530

    Create array _reverse.c
    Reversing elements of actual array

---
+++ 13-03-2024/array _reverse.c
@@ -0,0 +1,20 @@
+//Reversing elements of actual array
+#include <stdio.h>
+int main(){
+    int size;
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    int arr[size];
+    for(int i = 0; i < size; i++){
+        printf("Enter %d element of array: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0, j = size - 1; i< size/2, j< size/2; i++,j--){
+        arr[i] = arr[j];
+    }
+     printf("Reverse of array is: \n");
+    for(int i = size-1; i >= 0; i--){
+        printf("%d\t", arr[i]);
+    }
+    return 0;
+}
